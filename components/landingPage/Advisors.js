c40be915199039ecf250917665a4ad37eae5433e
Satye19 <Satyendra.sant7@gmail.com>
2022-07-02 17:44:56 +0530

    WIP : Advisors & testimonials Updated

--- components/landingPage/Advisors.js
+++ components/landingPage/Advisors.js
@@ -4,21 +4,22 @@ import { Autoplay, Keyboard, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Card, CardHeader, CardContent, Avatar, Box, Grid, Button, Typography } from '@mui/material';
+import { Card, CardHeader, CardContent, Stack, Avatar, Box, Grid, Button, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import VerifiedIcon from '@mui/icons-material/Verified';
 
-const CustomButton = styled(Button)({textTransform:'capitalize', fontSize:12, borderRadius:20, shadow:'none', fontFamily: 'poppins', fontWeight:400, width:105, backgroundColor:'#747A99', '&:hover': {backgroundColor:'#11193F',}});
-const SubscribeButton = styled(Button)({textTransform:'capitalize', fontSize:16, borderRadius:20, shadow:'none', fontFamily: 'poppins', fontWeight:800, color:'#FDAA3C', borderColor:'#FDAA3C', '&:hover': {borderColor:'#FDAA3C', backgroundColor:'#FDDDB2',}});
+const CustomButton = styled(Button)({textTransform:'capitalize', fontSize:12, borderRadius:20, fontFamily: 'poppins', width:104, backgroundColor:'#747A99', '&:hover': {backgroundColor:'#11193F',}});
+const SubscribeButton = styled(Button)({textTransform:'capitalize', fontSize:16, borderRadius:20, fontFamily: 'poppins', fontWeight:800, color:'#FDAA3C', borderColor:'#FDAA3C', '&:hover': {borderColor:'#FDAA3C', backgroundColor:'#FDDDB2',}});
 
-
+const DetailsButton = styled(Button)({textTransform:'capitalize', fontSize:12, borderRadius:20, fontFamily: 'poppins', width:110, backgroundColor:'#747A99', '&:hover': {backgroundColor:'#11193F',}});
+const RepresentativeButton = styled(Button)({textTransform:'capitalize', fontSize:12, borderRadius:20, fontFamily: 'poppins', width:240, backgroundColor:'#747A99', '&:hover': {backgroundColor:'#11193F',}});
 
 function AdvisorCard() {
 
   return (
-    <Card sx={{Height:330, p:1, borderRadius:8, display:'Block'}}>
+    <Card sx={{borderRadius:8, m:6, '& hover':{boxShadow:3,}}}>
       <CardHeader 
-        avatar={<Avatar alt="Remy Sharp" src="https://qph.cf2.quoracdn.net/main-qimg-54267778c97f83c195ad7b0efb64aaa9-lq" sx={{ width:98, height:120, borderRadius:6, }}/>} 
+        avatar={<Avatar src="https://qph.cf2.quoracdn.net/main-qimg-54267778c97f83c195ad7b0efb64aaa9-lq" sx={{ width:90, height:120, borderRadius:6, }}/>} 
         title={<Typography variant="h5" sx={{mb:1, ml:1, fontWeight:700,}}>Jack Williams</Typography>} 
         subheader={
           <Grid container spacing={1}>
@@ -43,19 +44,52 @@ function AdvisorCard() {
   );
 }
 
+function AllAdvisorCard() {
+  return (
+    <Card sx={{width:'65%', borderRadius:8, m:6, p:2, '& hover':{boxShadow:3,}}}>
+      <Grid container spacing={1}>
+        <Grid item xs={3} sx={{display:'flex', justifyContent:'center', alignItems:'center',}}>
+          <Avatar src="https://qph.cf2.quoracdn.net/main-qimg-54267778c97f83c195ad7b0efb64aaa9-lq" sx={{width:160, height:160, borderRadius:6,}} variant="rounded"/>
+        </Grid>
+        
+        <Grid item xs={9}>
+          <Typography variant="h5" sx={{mb:1, fontWeight:700,}}>Will Turner</Typography>
+          <Stack spacing={1} direction="row" sx={{mb:2,}}>
+            <DetailsButton variant="contained">3 Years Exp</DetailsButton>
+            <DetailsButton variant="contained">30+ Clients</DetailsButton>
+            <RepresentativeButton variant="contained">Investment Representative</RepresentativeButton>
+          </Stack>
+          <Typography variant="h6" color="text.secondary" sx={{fontWeight:700}}>About The Investor</Typography> 
+          <Typography variant="body2" color="text.secondary" sx={{mt:1}}>
+            started just a few years ago will minimumm capital, absolutely no experience, is earning millions everyday.  
+          </Typography>      
+        </Grid>
+        <Box sx={{width:'100%', display:'flex', justifyContent:'center', alignItems:'center', mb:2,}}>
+          <Stack spacing={2} direction="row>
+            <SubscribeButton variant="outlined">View Profile</SubscribeButton>
+            <SubscribeButton variant="outlined">Subscribe</SubscribeButton>
+          </Stack> 
+
+        </Box>
+
+      </Grid>
+    </Card>
+  );
+}
+
 export default function Advisors() {
   return (
     <Box sx={{p:4, backgroundColor:'#F6F6F7', display:'flex', alignItems:'center', justifyContent:'center',}}>
-      
-      <Box sx={{width:'90%'}}>
-        <Box sx={{width:'50%'}}>
+      <Box sx={{width:'97%'}}>
+
+        <Box sx={{width:'50%', ml:6,}}>
           <Typography variant="h4">Premium Advisors</Typography>
-          <p className="text-muted mb-4 mt-2 pb-2">
+          <p className="text-muted mb-2 mt-2">
             Get Insights and helpful suggestions from our premium and verified investors for effective shares management and financial planning.  
           </p>
         </Box>
         
-        <Swiper slidesPerView={3} spaceBetween={15} slidesPerGroup={3} autoplay={{delay:3000, disableOnInteraction:false,}} loop={true} keyboard={{enabled: true,}} loopFillGroupWithBlank={true} pagination={{clickable: true,}} navigation={true} modules={[Autoplay, Keyboard, Pagination, Navigation]} className="mySwiper">
+        <Swiper slidesPerView={3} spaceBetween={-46} slidesPerGroup={3} autoplay={{delay:3000, disableOnInteraction:false,}} loop={true} keyboard={{enabled: true,}} loopFillGroupWithBlank={true} pagination={{clickable: true,}} navigation={true} modules={[Autoplay, Keyboard, Pagination, Navigation]}>
           <SwiperSlide> <AdvisorCard/>  </SwiperSlide>
           <SwiperSlide> <AdvisorCard/>  </SwiperSlide>
           <SwiperSlide> <AdvisorCard/>  </SwiperSlide>
@@ -67,15 +101,20 @@ export default function Advisors() {
           <SwiperSlide> <AdvisorCard/>  </SwiperSlide>
         </Swiper>
 
-        <Box sx={{width:'50%'}}>
-          <Typography variant="h4">Premium Advisors</Typography>
+        <Box sx={{width:'50%', ml:6, mt:12,}}>
+          <Typography variant="h4">All Stock Advisors</Typography>
           <p className="text-muted mb-4 mt-2 pb-2">
             Get Insights and helpful suggestions from our premium and verified investors for effective shares management and financial planning.  
           </p>
         </Box>
 
-      </Box>
+        <Swiper slidesPerView={1} spaceBetween={0} slidesPerGroup={1} autoplay={{delay:3000, disableOnInteraction:false,}} loop={true} keyboard={{enabled: true,}} loopFillGroupWithBlank={true} pagination={{clickable: true,}} navigation={true} modules={[Autoplay, Keyboard, Pagination, Navigation]}>
+          <SwiperSlide> <AllAdvisorCard/>  </SwiperSlide>
+          <SwiperSlide> <AllAdvisorCard/>  </SwiperSlide>
+          <SwiperSlide> <AllAdvisorCard/>  </SwiperSlide>
+        </Swiper>
 
+      </Box>
     </Box>
   );
 };
--- components/landingPage/Testimonials.js
+++ components/landingPage/Testimonials.js
@@ -1,25 +1,30 @@
 import * as React from 'react';
-import { Card, CardHeader, CardContent, Avatar, Box, Typography } from '@mui/material'; 
+import { Card, CardHeader, CardContent, Avatar, Box, Rating, Typography } from '@mui/material'; 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Keyboard, Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 
 function TestimonialCard() {
 
   return (
-    <Card sx={{Width:345, m:2,}}>
+    <Card sx={{borderRadius:2, boxShadow:3, m:6,}}>
       <CardHeader
-        avatar={
-          <Avatar>
-            R
-          </Avatar>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        avatar={<Avatar src="https://st4.depositphotos.com/4157265/41100/i/450/depositphotos_411005388-stock-photo-profile-picture-of-smiling-30s.jpg" sx={{ width:60, height:60,}}/>} 
+        subheader={<Rating name="size-large" defaultValue={5} size="large" />}
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
+        <Box>
+          <Typography variant="body" color="text.secondary">It is a great platform for investment. Interactive and clean User Interface. Features like creating your own portfolio are great..I started investing in Mutual Funds.</Typography>
+        </Box>
+        <Box sx={{mt:3}}>
+          <Typography variant="h7" sx={{fontWeight:700}}>John Doe</Typography>
+        </Box>
+        <Box>
+          <Typography variant="body" sx={{fontSize:14}}>Product Manager, Google</Typography>
+        </Box>  
       </CardContent>
     </Card>
   );
@@ -27,16 +32,26 @@ function TestimonialCard() {
 
 export default function Testimonials() {
   return (
-    <Box sx={{display:'flex', alignItems:'center',  justifyContent:'center',}}>
-      <Box sx={{m:2, p:2,}}> 
-        <h1 className="mb-4 font-weight-normal line-height-1_4">What People Say About Us</h1>
-        <p className="text-muted mb-4 pb-2">We help you see the world differently, discover opportunities you may never have imagined and achieve results that bridge what is with what can be.</p>
-        
-        <Box sx={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center',}}>
-          <TestimonialCard/> 
-          <TestimonialCard/> 
-          <TestimonialCard/> 
+    <Box sx={{display:'flex', alignItems:'center', justifyContent:'center', p:4, mt:8,}}>
+      <Box sx={{width:'97%'}}> 
+
+        <Box sx={{width:'50%', ml:6,}}>
+          <Typography variant="h4">What People Say About Us</Typography>
+          <p className="text-muted mb-2 mt-2">We help you see the world differently, discover opportunities you may never have imagined and achieve results that bridge what is with what can be.</p>
         </Box>
+
+        <Swiper slidesPerView={3} spaceBetween={-40} slidesPerGroup={3} autoplay={{delay:3000, disableOnInteraction:false,}} loop={true} keyboard={{enabled: true,}} loopFillGroupWithBlank={true} pagination={{clickable: true,}} navigation={true} modules={[Autoplay, Keyboard, Pagination, Navigation]}>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+          <SwiperSlide> <TestimonialCard/>  </SwiperSlide>
+        </Swiper>
+
       </Box>
     </Box>
   );
